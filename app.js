74735b258a8507157fd95acd17aa19af8cd349e5
Sukomal07 <hypersukomal@gmail.com>
2023-01-09 19:48:27 +0530

    First commit

---
+++ app.js
@@ -0,0 +1,25 @@
+const mailer = require('nodemailer')
+
+let transport = mailer.createTransport({
+    service:'gmail',
+    auth:{
+        user:'hypersukomal@gmail.com',
+        pass:''
+    }
+})
+
+let message = {
+    from:'hypersukomal@gmail.com',
+    to:'haren0610@gmail.com',
+    subject:'Testing Email using Node.js',
+    text:'Thia mail is sending from Node.js'
+}
+
+transport.sendMail(message, (error , info) =>{
+    if(error){
+        console.log(error)
+    }else{
+        console.log("Email send")
+        console.log(info.response)
+    }
+})
